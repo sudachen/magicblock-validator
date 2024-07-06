e28a29a2cd1495e6ddd2c2f1d66d262596a73fab
Thorsten Lorenz <thlorenz@gmx.de>
2024-07-06 01:02:26 -0700

    feat: add address lookup table builtin (#82)
    ## Summary
    
    Adding address lookup table program to loaded builtins and adding docs
    about un/supported
    builtins.
    
    ## Details
    
    After adding address lookup table program we support the below explained
    builtins:
    
    _Quoting from README.md:_
    
    We support and load the following builtin programs at startup:
    
    - `system_program`
    - `solana_bpf_loader_upgradeable_program`
    - `compute_budget_program`
    - `address_lookup_table_program`
    - `sleipnir_program` which supports account mutations, etc.
    
    We don't support the following builtin programs:
    
    - `vote_program` since we have no votes
    - `stake_program` since we don't support staking in our validator
    - `config_program` since we don't support configuration (_Add
    configuration data to the chain and the
    list of public keys that are permitted to modify it_)
    - `solana_bpf_loader_deprecated_program` because it's deprecated
    - `solana_bpf_loader_program` since we use the
    `solana_bpf_loader_upgradeable_program` instead
    - `zk_token_proof_program` it's behind a feature flag
    (`feature_set::zk_token_sdk_enabled`) in
      the solana validator and we don't support it yet
    - `solana_sdk::loader_v4` it's behind a feature flag
    (`feature_set::enable_program_runtime_v2_and_loader_v4`) in the solana
      validator and we don't support it yet

--- sleipnir-bank/README.md
+++ sleipnir-bank/README.md
@@ -1,11 +1,10 @@
-
-# Summary
+## Summary
 
 The `Bank` is responsible for holding account states and preparing transactions
 that are then executed inside the SVM. The SVM is implemented in its own crate.
 The `Bank` also does post processing to update state after the transaction ran inside the SVM
 
-# Details
+## Details
 
 *Important symbols:*
 
@@ -23,7 +22,30 @@ The `Bank` also does post processing to update state after the transaction ran i
   - It's basically a `HashMap<Hash, (Slot, HashMap<Key, Vec<(Slot, T)>>)>`
   - // TODO(vbrunet) - figure out exactly how data structure works
 
-# Notes
+### Builtin Programs
+
+We support and load the following builtin programs at startup:
+
+- `system_program`
+- `solana_bpf_loader_upgradeable_program`
+- `compute_budget_program`
+- `address_lookup_table_program`
+- `sleipnir_program` which supports account mutations, etc.
+
+We don't support the following builtin programs:
+
+- `vote_program` since we have no votes
+- `stake_program` since we don't support staking in our validator
+- `config_program` since we don't support configuration (_Add configuration data to the chain and the
+list of public keys that are permitted to modify it_)
+- `solana_bpf_loader_deprecated_program` because it's deprecated
+- `solana_bpf_loader_program` since we use the `solana_bpf_loader_upgradeable_program` instead
+- `zk_token_proof_program` it's behind a feature flag (`feature_set::zk_token_sdk_enabled`) in
+  the solana validator and we don't support it yet
+- `solana_sdk::loader_v4` it's behind a feature flag (`feature_set::enable_program_runtime_v2_and_loader_v4`) in the solana
+  validator and we don't support it yet
+
+## Notes
 
 `Bank` implements `AddressLoader`, used to sanitize transactions.
 
--- sleipnir-bank/src/builtins.rs
+++ sleipnir-bank/src/builtins.rs
@@ -1,6 +1,9 @@
 // NOTE: copied from runtime/src/builtins.rs
 use solana_program_runtime::invoke_context::BuiltinFunctionWithContext;
-use solana_sdk::{bpf_loader_upgradeable, compute_budget, pubkey::Pubkey};
+use solana_sdk::{
+    address_lookup_table, bpf_loader_upgradeable, compute_budget,
+    pubkey::Pubkey,
+};
 
 pub struct BuiltinPrototype {
     pub feature_id: Option<Pubkey>,
@@ -40,6 +43,27 @@ impl solana_frozen_abi::abi_example::AbiExample for BuiltinPrototype {
     }
 }
 
+/// We support and load the following builtin programs at startup:
+///
+/// - `system_program`
+/// - `solana_bpf_loader_upgradeable_program`
+/// - `compute_budget_program"t`
+/// - `address_lookup_table_program`
+/// - `sleipnir_program` which supports account mutations, etc.
+///
+/// We don't support the following builtin programs:
+///
+/// - `vote_program` since we have no votes
+/// - `stake_program` since we don't support staking in our validator
+/// - `config_program` since we don't support configuration (_Add configuration data to the chain and the
+/// list of public keys that are permitted to modify it_)
+/// - `solana_bpf_loader_deprecated_program` because it's deprecated
+/// - `solana_bpf_loader_program` since we use the `solana_bpf_loader_upgradeable_program` instead
+/// - `zk_token_proof_program` it's behind a feature flag (`feature_set::zk_token_sdk_enabled`) in
+///   the solana validator and we don't support it yet
+/// - `solana_sdk::loader_v4` it's behind a feature flag (`feature_set::enable_program_runtime_v2_and_loader_v4`) in the solana
+///   validator and we don't support it yet
+/// See: solana repo - runtime/src/builtins.rs
 pub static BUILTINS: &[BuiltinPrototype] = &[
     BuiltinPrototype {
         feature_id: None,
@@ -64,5 +88,12 @@ pub static BUILTINS: &[BuiltinPrototype] = &[
         program_id: compute_budget::id(),
         name: "compute_budget_program",
         entrypoint: solana_compute_budget_program::Entrypoint::vm,
+    },
+    BuiltinPrototype {
+        feature_id: None,
+        program_id: address_lookup_table::program::id(),
+        name: "address_lookup_table_program",
+        entrypoint:
+            solana_address_lookup_table_program::processor::Entrypoint::vm,
     },
 ];
